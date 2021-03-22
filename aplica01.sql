746843039651492ab389b1972e2c90a2b0879f4c
JUAN GABRIEL CATUCHE GIRON <jcatuche@contratista.epm.co>
2021-03-22 14:47:26 -0500

    Prueba version 2

---
+++ aplica01.sql
@@ -0,0 +1,13 @@
+--
+--
+--[Ticket] es el numero de WO que se va a desplegar
+--[aplica del esquema] e
+PROMPT Script de aplicacion General
+PROMPT Inicia Ejecucion WO:WO0000000958309
+
+PROMPT Ejecutando archivo WO0000000958309_apl_flex_01.sql
+@./WO0000000958309_apl_flex_01.sql
+
+PROMPT Termina Ejecucion aplicacion General
+
+EXIT
